335a93de2922a3e93f31a324e6fc537e8a7086c4
jackkitte <jackkitte735@gmail.com>
2020-02-02 23:05:35 +0900

    ルーティングの登録

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/jackkitte/youtube_manager_go/routes"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+	routes.Init(e)
 	e.Logger.Fatal(e.Start(":8888"))
 }
