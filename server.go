10fc127f66d8b4f0a40ba4af86e7c9d33874be31
jackkitte <jackkitte735@gmail.com>
2020-02-03 01:09:52 +0900

    ログ出力用の設定

--- server.go
+++ server.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/jackkitte/youtube_manager_go/routes"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	routes.Init(e)
 	e.Logger.Fatal(e.Start(":8888"))
 }
