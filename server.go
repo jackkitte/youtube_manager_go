d5a48d0b59e7f99f290333a9b3c2348628edbd1a
jackkitte <jackkitte735@gmail.com>
2020-02-03 01:51:15 +0900

    api keyを環境変数として取得できるように変更（commitした以前のapi keyも変更）

--- server.go
+++ server.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"github.com/jackkitte/youtube_manager_go/routes"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		logrus.Fatal("Error loading .env")
+	}
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetFormatter(&logrus.JSONFormatter{})
 }
+
 func main() {
 	e := echo.New()
+
+	//Middleware
 	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+
+	//Routes
 	routes.Init(e)
+
 	e.Logger.Fatal(e.Start(":8888"))
 }
