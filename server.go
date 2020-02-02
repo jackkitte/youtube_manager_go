46304d0ccb5252c5ed023d3d7fa4fd9675b64712
jackkitte <jackkitte735@gmail.com>
2020-02-03 01:41:01 +0900

    ログのフォーマット設定とyoutube apiを使った動画取得

--- server.go
+++ server.go
@@ -4,8 +4,13 @@ import (
 	"github.com/jackkitte/youtube_manager_go/routes"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/sirupsen/logrus"
 )
 
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
